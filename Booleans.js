4097f75756025f90eb1e8ef9b0476b87fb719f3c
poorviii19 <shuklapoorviswe@gmail.com>
2026-04-20 00:41:11 +0530

    Booleans, If-Else, Coparators

---
+++ Booleans.js
@@ -0,0 +1,80 @@
+// Booleans have only two values: true and false.
+console.log(3<5)  //output is a boolean value: true
+console.log(3>5); //output: false
+
+// 'true' 'false'  wrong way , converts to string
+console.log(typeof 'true');  //string
+console.log(typeof true);  //boolean
+
+
+/*comparison operators:
+ >greater than
+< less than
+>= greater than or equals to
+<= less than or equals to
+== equals to tries to conver both values into same type
+=== equals to
+!= not equals to tries to conver both values into same type
+!== not equals to
+ */
+
+console.log(5 == '5.00');  //wrong way because they shouldn't really be equal to each other
+
+console.log(3==='3'); //right way
+
+console.log(3>5-5);  //comparison operators have a lower priority than mathematical operator
+
+// If-Statements:
+
+
+// if(condition1){
+//     code
+// }
+// else if(condition2){
+//     code
+// }
+// else{
+//     code
+// }
+
+
+if(true){
+    console.log('hello');
+}  //it will work, displays on console
+if(false){
+    console.log('hello');
+}  //it will not work, does not display on console
+
+
+
+// If-else:
+
+if(false){
+    console.log("hello World");
+}
+else{
+    console.log('value is false');
+}
+
+
+// Check the age of person to drive:
+if(13>=18){
+    console.log("You can drive!");
+}
+else{
+    console.log("you can not drive!");
+}
+
+
+
+const age = 16;
+
+if(age>=18){
+    console.log("you are eligible to drive");
+}
+else if(age>=16){
+    console.log("you can drive as a trainee");
+}
+else{
+    console.log("you are not eligible!");
+}
